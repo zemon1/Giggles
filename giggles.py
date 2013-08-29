431981f6d3ac9012e4a0b6277741b6211fd94fca
Jeff Haak <zemon1@csh.rit.edu>
2013-08-29 19:51:36 -0400

    Quick and dirty so far

--- giggles.py
+++ giggles.py
@@ -2,25 +2,28 @@
 #Harlan Haskins
 import random
 
+popId = 0
 
+def mate(pop, count):
+     global popId
 
+     if count >= 5:
+         return pop
 
+     else:
+         new = []
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+         if len(pop) == 0:
+             pop.append(popId)
+             pop.append(popId + 1)
+             popId += 2
+         else:
+             for ele in pop:
+                 new.append(popId)
+                 new.append(popId + 1)
+                 popId += 2
+             pop.extend(new)
+         mate(pop, count + 1)
 
 if __name__ == "__main__":
-    print "hello"
+    print mate([], 0)
