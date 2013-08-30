ed5d7df7b26bec6ebf77bcac99df99de4f2d6f36
Jeff Haak <zemon1@csh.rit.edu>
2013-08-29 20:56:02 -0400

    Seems to work

--- giggles.py
+++ giggles.py
@@ -7,8 +7,9 @@ popId = 0
 def mate(pop):
      global popId
      
+     #print "Pop:", pop
      if len(pop) == 0:
-         pop.append([popId, 0]) #Giggles
+         pop.append([popId, 0, 0]) #Giggles
          popId += 1
      
      elif pop[0][1] > 4:
@@ -18,16 +19,30 @@ def mate(pop):
          new = []
 
          for ele in pop:
+             #ele[1] += 1 #do they age before or after the loop??
              if ele[1] > 0 and ele[1] < 6:
-                new.append([popId, 0])
-                new.append([popId + 1, 0])
+                new.append([popId, 0, ele[0]])
+                new.append([popId + 1, 0, ele[0]])
                 popId += 2
-             ele[1] += 1
+             ele[1] += 1 #do they age before or after the loop??
          pop.extend(new)
          
      mate(pop)
      return pop
 
 if __name__ == "__main__":
-    end = mate([])
-    print end
+     random.seed(1)
+     end = mate([])
+     thresh = 30
+     meet = []
+
+     for pup in end:
+          
+          chance = random.randrange(1, 100)
+          if (chance <= thresh) and not pup == end[0]:
+               meet.append(pup)     
+     
+     print len(meet) 
+     print meet
+     print "\n----------------------------------\n"
+     print end
